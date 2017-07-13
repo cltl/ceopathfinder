7ba57a0a57db83a2bc94b9b6fe01ff78a5fb261b
piekvossen <piek.vossen@vu.nl>
2017-07-13 22:00:50 +0200

    -- first commit

--- scripts/ceopath.sh
+++ scripts/ceopath.sh
@@ -11,3 +11,8 @@ java -Xmx812m -cp "$LIB/ceopathfinder-v0.1-jar-with-dependencies.jar" CeoPathFin
 java -Xmx812m -cp "$LIB/ceopathfinder-v0.1-jar-with-dependencies.jar" CeoPathFinder --ont-file "$RESOURCES/CEO_ESO.owl" --match 0 --e1 Fire --e2 ExtinguishingFire
 
 java -Xmx812m -cp "$LIB/ceopathfinder-v0.1-jar-with-dependencies.jar" CeoPathFinder --ont-file "$RESOURCES/CEO_ESO.owl" --printChain > chain.txt
+
+# special parameters can be used in combination with the function for comparing two events:
+# --deep pre, during and post conditions are inherited along the type hierarchy from top to bottom
+# --match [0,1] 0 means only direct matches, 1 means only when there is one intermediate class
+# --during also during conditions are considered as pre and post
